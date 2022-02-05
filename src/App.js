97f030ec3394ea7bdca42d6168937822972f9557
pylypchukroman <romanpylypchuk@gmail.com>
2022-02-05 18:53:27 +0200

    додає базові компоненти, без форми поки що

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import data from "./Data/Data.json";
+import React, { useState } from "react";
+//components
+import Header from "./Header/Header";
+import ToDoList from "./TodoList/ToDoList.jsx";
+
+import "./App.css";
+
+function App() {
+  const [toDoList, setToDoList] = useState(data);
+
+  const handleToggle = (id) => {
+    let mapped = toDoList.map((task) => {
+      return task.id == id
+        ? { ...task, complete: !task.complete }
+        : { ...task };
+    });
+    setToDoList(mapped);
+  };
+
+  const handleFilter = () => {
+    let filtered = toDoList.filter((task) => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <ToDoList
+        toDoList={toDoList}
+        handleToggle={handleToggle}
+        handleFilter={handleFilter}
+      />
+    </div>
+  );
+}
+
+export default App;
